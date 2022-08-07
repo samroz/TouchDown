281a95758e82564c4bd551dccc767cf90a0d2ccc
Samroz Javed <samroz.javed@gmail.com>
2022-08-07 13:29:23 +0200

    Initial Commit

---
+++ TouchDown/TouchDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownApp.swift
+//  TouchDown
+//
+//  Created by Samroz Javed on 07/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
